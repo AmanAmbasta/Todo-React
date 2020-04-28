a3b2ebc67d4981adca8d3eca94228a3ce2297221
AmanAmbasta <64021113+AmanAmbasta@users.noreply.github.com>
2020-04-29 04:25:01 +0530

    First commit

---
+++ src/todoData.js
@@ -0,0 +1,32 @@
+const todoData =[
+    {
+        id: 1,
+        text: "wake in the morning",
+        completed: true
+    },
+    
+    {
+        id: 2,
+        text: "Brush your teeth",
+        completed: false
+    },
+    
+    {
+        id: 3,
+        text: "take breakfast",
+        completed: false
+    },
+    
+    {
+        id: 4,
+        text: "move around",
+        completed: true
+    },
+
+    {
+        id: 5,
+        text: "Start work",
+        completed: false
+    }
+];
+export default todoData;
